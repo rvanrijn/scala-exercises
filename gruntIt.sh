58bca058eff8a611abd6e04b730c4371913da0e7
rvanrijn <r.van.rijn@ncim.nl>
2015-10-18 19:32:18 +0200

    added bash script

--- gruntIt.sh
+++ gruntIt.sh
@@ -3,6 +3,8 @@ echo Running grunt
 cd /scala-exercises/test
 bower install -allow-root
 cd /scala-exercises/
+locale-gen "en_US.UTF-8"
+dpkg-reconfigure locales
+gem install sass
 grunt wiredep
 grunt serve    
-        
